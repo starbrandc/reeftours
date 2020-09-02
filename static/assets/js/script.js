b94240b99eae5be09276de6efb3b9b717c261589
sam <retina@code.local>
2020-09-02 13:31:50 +0300

    we up there

--- static/assets/js/script.js
+++ static/assets/js/script.js
@@ -614,6 +614,10 @@
             rewind: true,
             dots: true,
             nav:true,
+            navText: [
+                '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+                '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+            ],
             responsive: {
                 0: {
                     items: 1
